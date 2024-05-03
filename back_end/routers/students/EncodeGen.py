f7cbe2452843e2f4faa2ca7aa0e86f0a02d605ef
misterkwao <vigilantkwao@gmail.com>
2024-05-03 12:04:50 +0000

    koyeb file test

--- back_end/routers/students/EncodeGen.py
+++ back_end/routers/students/EncodeGen.py
@@ -19,30 +19,30 @@ def face_encode(image,filename,student_college,student_year_enrolled):
 
     # loading existing joblib file to append new encoding
 
-    try:
-        file = open(f"Encodings/{student_college}_{student_year_enrolled}.joblib", 'rb')
-        facialencodingsWithNames = joblib.load(file)
-        encoding, student_names = facialencodingsWithNames
-        file.close()
+    # try:
+    #     file = open(f"Encodings/{student_college}_{student_year_enrolled}.joblib", 'rb')
+    #     facialencodingsWithNames = joblib.load(file)
+    #     encoding, student_names = facialencodingsWithNames
+    #     file.close()
 
-        # appending new encoding
-        updated_encodings =[]
-        updated_encodings.append(encoding) #Appending already existing encodings
-        updated_encodings.append(new_encoding) #Appending new encoding for new student
+    #     # appending new encoding
+    #     updated_encodings =[]
+    #     updated_encodings.append(encoding) #Appending already existing encodings
+    #     updated_encodings.append(new_encoding) #Appending new encoding for new student
 
-        updated_names =[]
-        updated_names.append(student_names) #Appending already existing names
-        updated_names.append(new_student_name) #Appending new name for new student
+    #     updated_names =[]
+    #     updated_names.append(student_names) #Appending already existing names
+    #     updated_names.append(new_student_name) #Appending new name for new student
 
-        # saving new details
-        facialencodingsWithNames = [updated_encodings, updated_names]
-        file = open(f"Encodings/{student_college}_{student_year_enrolled}.joblib", 'wb')
-        joblib.dump(facialencodingsWithNames, file)
-        file.close()
-        return True #This means the file was Updated successfully
-    except:
-        facialencodingsWithNames = [new_encoding, new_student_name]
-        file = open(f"Encodings/{student_college}_{student_year_enrolled}.joblib", 'wb')
-        joblib.dump(facialencodingsWithNames, file)
-        file.close()
-        return True #This means the file was successfully saved
+    #     # saving new details
+    #     facialencodingsWithNames = [updated_encodings, updated_names]
+    #     file = open(f"Encodings/{student_college}_{student_year_enrolled}.joblib", 'wb')
+    #     joblib.dump(facialencodingsWithNames, file)
+    #     file.close()
+    #     return True #This means the file was Updated successfully
+    # except:
+    facialencodingsWithNames = [new_encoding, new_student_name]
+    file = open(f"Encodings/{student_college}_{student_year_enrolled}.joblib", 'wb')
+    joblib.dump(facialencodingsWithNames, file)
+    file.close()
+    return True #This means the file was successfully saved
